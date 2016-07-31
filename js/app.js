4e191dc6d0f033130587d0917f706459355efdf4
youssefadel94 <darkmetalxdragon@hotmail.com>
2016-07-31 16:57:50 +0200

    setup 0.5

--- js/app.js
+++ js/app.js
@@ -1,4 +1,21 @@
-var app = angular.module('myApp', ['ngRoute', 'ngMessages'])
+var app = angular.module('myApp', ['ngRoute', 'ngMessages']) //configure routes
     .config(function($routeProvider) {
-
+        $routeProvider
+            .when('/', {
+                templateUrl: 'views/home.html',
+                controller: 'HomeCtrl'
+            })
+            .when('/login', {
+                templateUrl: 'views/login.html',
+                controller: 'LoginCtrl'
+            })
+            .when('/signup', {
+                templateUrl: 'views/signup.html',
+                controller: 'SignupCtrl'
+            })
+            .when('/photo/:id', {
+                templateUrl: 'views/detail.html',
+                controller: 'DetailCtrl'
+            })
+            .otherwise('/');
     });
