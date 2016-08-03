63fb6a6c058b768c3ece0f8a77c613e3a4ddf1f5
youssefadel94 <darkmetalxdragon@hotmail.com>
2016-08-03 23:35:19 +0200

    auth throuhg insta

--- client/controllers/home.js
+++ client/controllers/home.js
@@ -2,11 +2,15 @@ app
     .controller('HomeCtrl', function($scope, $window, $rootScope, $auth) {
 
         $scope.isAuthenticated = function() {
-            // check if logged in
+            return $auth.isAuthenticated();
         };
 
         $scope.linkInstagram = function() {
-            // connect email account with instagram
+            $auth.link('instagram')
+                .then(function(response) {
+                    $window.localStorage.currentUser = JSON.stringify(response.data.user);
+                    $rootScope.currentUser = JSON.parse($window.localStorage.currentUser);
+                });
         };
 
     });
